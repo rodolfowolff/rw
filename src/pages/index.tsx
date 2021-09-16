9b5b95fdc33683dea84b777954cc443f44b8a0a5
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-09-16 11:45:47 -0300

    add global style css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import Head from 'next/head'
 
-import styles from '../styles/home.module.scss'
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Home | rw.news</title>
       </Head>
-      <h1 className={styles.title}>Hello world!</h1>
+      <h1>Hello world!</h1>
     </>
   )
 }
