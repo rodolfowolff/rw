cdc3665994229b8731a1cd7f7ece15395e6b80dd
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-09-16 12:17:03 -0300

    add header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+
+import styles from './styles.module.scss'
+
+export default function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image src="/images/logo.svg" alt="rw.news" width="80px" height="30px" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
