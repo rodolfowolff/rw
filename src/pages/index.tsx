bea999eb9f97f8d67fa2b05ed134f353fd18b28c
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-09-16 11:31:50 -0300

    add title dynamic page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
+import Head from 'next/head'
+
 import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
-    <div>
+    <>
+      <Head>
+        <title>Home | rw.news</title>
+      </Head>
       <h1 className={styles.title}>Hello world!</h1>
-    </div>
+    </>
   )
 }
